a5cab768107ae88aa516aae9a9a389c1e9d5a8c5
Ivan Carvalho <ivan.carvalho@alumni.ubc.ca>
2020-11-15 20:03:29 -0800

    Add listorder.js
    Implement everything for Q1 except formatting

--- routes/listorder.js
+++ routes/listorder.js
@@ -5,27 +5,80 @@ const moment = require('moment');
 
 router.get('/', function(req, res, next) {
     res.setHeader('Content-Type', 'text/html');
-    res.write('<title>YOUR NAME Grocery Order List</title>');
+    res.write('<title>DBs and Dragons Grocery Order List</title>');
 
-    /** Create connection, and validate that it connected successfully **/
+    (async function() {
+        try {
+            let pool = await sql.connect(dbConfig);
 
-    /**
-    Useful code for formatting currency:
-        let num = 2.87879778;
-        num = num.toFixed(2);
-    **/
+            let sqlQuery = `
+                SELECT 
+                    ordersummary.orderId, 
+                    orderDate,
+                    customer.customerId,
+                    CONCAT(customer.firstName, ' ', customer.lastName) AS customerName,
+                    COALESCE(SUM(price), 0) AS total
+                FROM ordersummary
+                INNER JOIN customer
+                ON ordersummary.customerId = customer.customerId
+                INNER JOIN orderproduct
+                ON ordersummary.orderId = orderproduct.orderId
+                GROUP BY 
+                    ordersummary.orderId, 
+                    orderDate, customer.customerId, 
+                    CONCAT(customer.firstName, ' ', customer.lastName)
+            `;
 
-    /** Write query to retrieve all order headers **/
+            let subQuery = `
+                SELECT
+                    productId,
+                    quantity,
+                    price
+                FROM orderproduct
+                WHERE orderId = @param
+            `;
 
-    /** For each order in the results
-            Print out the order header information
-            Write a query to retrieve the products in the order
+            let results = await pool.request().query(sqlQuery);
+            
+            let dateFormatOptions = { 
+                weekday: 'long', 
+                year: 'numeric', 
+                month: 'long', 
+                day: 'numeric', 
+                hour: '2-digit', 
+                minute: '2-digit', 
+                second: '2-digit' 
+            };
 
-            For each product in the order
-                Write out product information 
-    **/
+            for (let i = 0; i < results.recordset.length; i++) {
+                let result = results.recordset[i];
+                let formattedDate = result.orderDate.toLocaleDateString("en-US", dateFormatOptions);
 
-    res.end();
+                const ps = new sql.PreparedStatement(pool);
+                ps.input('param', sql.Int);
+                await ps.prepare(subQuery);
+
+                let subResults = await ps.execute({param: result.orderId});
+                let subResult = subResults.recordset;
+
+                res.write(`orderId ${result.orderId} ${formattedDate} ${result.customerName} ${result.total} <br>`);
+
+                subResults.recordset.forEach( (subResult) => {
+                    res.write(`aaaaaaaaa         productId ${subResult.productId} ${subResult.quantity} ${subResult.price.toFixed(2)} <br>`);
+                });
+
+                res.write(`<br>`);
+
+            };
+
+        } catch(err) {
+            console.dir(err);
+            res.write(err)
+        }
+        finally {
+            res.end();
+        }
+    })();
 });
 
 module.exports = router;
