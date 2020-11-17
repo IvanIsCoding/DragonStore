83d8f16f883152cad5860d33dd7061b2dd775804
Ivan Carvalho <ivan.carvalho@alumni.ubc.ca>
2020-11-16 19:16:55 -0800

    Finish Q1
    Add HTML table formatting

--- routes/listorder.js
+++ routes/listorder.js
@@ -4,6 +4,73 @@ const sql = require('mssql');
 const moment = require('moment');
 
 router.get('/', function(req, res, next) {
+
+    let createProductRow = (subResult) => {
+        return `
+        <tr> 
+            <td>${subResult.productId}</td> <td>${subResult.quantity}</td> <td>${subResult.price.toFixed(2)}</td> 
+        </tr>
+        `;
+    };
+
+    let createProducts = (subResults) => {
+        return subResults.map(createProductRow).join('\n');
+    };
+
+    let createProductTable = (subResults) => {
+        return `
+        <tr align="right">
+            <td colspan="5">
+                <table border="1">
+                    <th>Product Id</th> <th>Quantity</th> <th>Price</th>
+                    ${createProducts(subResults)}
+                </table>
+            </td>
+        </tr>
+        `;
+    };
+
+    let createTableRow = (data) => {
+        
+        let result = data.result;
+        
+        let dateFormatOptions = { 
+                weekday: 'long', 
+                year: 'numeric', 
+                month: 'long', 
+                day: 'numeric', 
+                hour: '2-digit', 
+                minute: '2-digit', 
+                second: '2-digit' 
+        };
+        let formattedDate = result.orderDate.toLocaleDateString("en-US", dateFormatOptions);
+
+        return `
+        <tr> 
+            <td>${result.orderId}</td> <td>${formattedDate}</td> <td>${result.customerId}</td> 
+            <td>${result.customerName}</td> <td>\$${result.total.toFixed(2)}</td>
+            ${createProductTable(data.subResults)}
+        </tr>
+        `;
+    };
+
+    let createRows = (orderListData) => {
+        return orderListData.map(createTableRow).join('\n');
+    };
+
+    let writeTable = (res, orderListData) => {
+        res.write(
+            `
+            <table border=1>
+                <tr> 
+                    <th>Order Id</th> <th>Order Date</th> <th>Customer Id</th> <th>Customer Name</th> <th>Total Amount</th> 
+                </tr>
+                ${createRows(orderListData)}
+            </table>
+            `
+        );
+    };
+
     res.setHeader('Content-Type', 'text/html');
     res.write('<title>DBs and Dragons Grocery Order List</title>');
 
@@ -39,37 +106,21 @@ router.get('/', function(req, res, next) {
             `;
 
             let results = await pool.request().query(sqlQuery);
-            
-            let dateFormatOptions = { 
-                weekday: 'long', 
-                year: 'numeric', 
-                month: 'long', 
-                day: 'numeric', 
-                hour: '2-digit', 
-                minute: '2-digit', 
-                second: '2-digit' 
-            };
+            let orderListData = [];
 
-            for (let i = 0; i < results.recordset.length; i++) {
-                let result = results.recordset[i];
-                let formattedDate = result.orderDate.toLocaleDateString("en-US", dateFormatOptions);
+            for (let result of results.recordset) {
 
                 const ps = new sql.PreparedStatement(pool);
                 ps.input('param', sql.Int);
                 await ps.prepare(subQuery);
 
                 let subResults = await ps.execute({param: result.orderId});
-                let subResult = subResults.recordset;
 
-                res.write(`orderId ${result.orderId} ${formattedDate} ${result.customerName} ${result.total} <br>`);
-
-                subResults.recordset.forEach( (subResult) => {
-                    res.write(`aaaaaaaaa         productId ${subResult.productId} ${subResult.quantity} ${subResult.price.toFixed(2)} <br>`);
-                });
-
-                res.write(`<br>`);
+                orderListData.push({'result': result, 'subResults': subResults.recordset});
 
             };
+
+            writeTable(res, orderListData);
 
         } catch(err) {
             console.dir(err);
