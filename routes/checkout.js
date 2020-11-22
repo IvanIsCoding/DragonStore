b14b026482a6d666c75974ebff6b22a6287023c3
Ivan Carvalho <ivan.carvalho@alumni.ubc.ca>
2020-11-22 11:05:43 -0800

    Refactor checkout.js

--- routes/checkout.js
+++ routes/checkout.js
@@ -1,52 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const writeHeader = require('../shared_functions/header');
 
 router.get('/', function(req, res, next) {
 
-    writeHeader(res, `DBs and Dragons Grocery CheckOut Line`, `checkout`);
+    let isInvalidPassword = req.session.invalidPassword;
 
-    /* Start of utilities to write checkout page */
-    let createWarning = (isInvalid) => {
-    	if (isInvalid) {
-    		return `
-    			<b>At least one of the user or password you entered was not correct. Please try again. </b> 
-    			<br>
-    			<br>
-    		`;
-    	}
-    	return '';
-    };
-
-    let writeCheckout = (res, isInvalid) => {
-	    res.write(
-	    	`
-	    	<h1>Enter your customer id and password to complete the transaction:</h1>
-	    	${createWarning(isInvalid)}
-			<form method="post" action="customerauthentication">
-				<table>
-					<tr>
-						<td>Customer ID:</td><td><input type="text" name="customerId" size="20"></td>
-					</tr>
-					<tr>
-						<td>Password:</td><td><input type="password" name="password" size="30"></td>
-					</tr>
-					<tr>
-						<td> <input type="submit" value="Submit"> </td> <td> <input type="reset" value="Reset"> </td>
-					</tr>
-				</table>
-			</form>
-	    	`
-	    );
-	};
-	/* End of utilities to write checkout page */
-
-	writeCheckout(res, req.session.invalidPassword);
-    
     if(req.session.invalidPassword) {
 		req.session.invalidPassword = false;	
 	}
-    res.end();
+    
+    res.render('checkout', {
+        title: 'DBs and Dragons Grocery Order List',
+        pageActive: {'checkout': true},
+        isInvalid: isInvalidPassword,
+        helpers: {}
+    });
 
 });
 
