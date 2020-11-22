628fae98f1aa58d365310b1cfd30429444c7a493
Ivan Carvalho <ivan.carvalho@alumni.ubc.ca>
2020-11-22 09:55:47 -0300

    Highlight active page in header (#13)

--- shared_functions/header.js
+++ shared_functions/header.js
@@ -1,7 +1,32 @@
+const createSingleHyperlink = (hyperlink) => {
+    let classString = '';
+
+    if (hyperlink.active) {
+        classString = `class = "active"`;
+    }
+
+    return `<a ${classString} href="${hyperlink.path}">${hyperlink.text}</a>`;  
+};
+
+const createHyperlinks = (hyperlinks) => {
+    return hyperlinks.map(createSingleHyperlink).join('\n');
+};
+
 const writeHeader = (res, title, currentPage, setHeader=true) => {
 
 	if (setHeader) {
 		res.setHeader('Content-Type', 'text/html');
+    }
+
+    const hyperlinks = [
+        {'text': 'Home', 'path': '/'},
+        {'text': 'Cart', 'path': 'showcart'},
+        {'text': 'Products', 'path': 'listprod'},
+        {'text': 'Orders', 'path': 'listorder'}
+    ];
+
+    for(let hyperlink of hyperlinks) {
+        hyperlink.active = (hyperlink.path === currentPage);
     }
 
     res.write(
@@ -9,10 +34,7 @@ const writeHeader = (res, title, currentPage, setHeader=true) => {
         <link rel="stylesheet" href="css/style.css">
         <div id="navbar">
             <img src="images/banner.png" alt="DBs & Dragons">
-            <a href="/">Home</a>
-            <a href="showcart">Cart</a>
-            <a href="listprod">Products</a>
-            <a href="listorder">Orders</a>
+            ${createHyperlinks(hyperlinks)}
         </div>
         <br>
         <br>
