78f63e7ee923d81201a919117122148174899870
Ivan Carvalho <ivan.carvalho@alumni.ubc.ca>
2020-11-22 12:21:00 -0800

    Refactor showcart.js

--- routes/showcart.js
+++ routes/showcart.js
@@ -2,134 +2,64 @@ const express = require('express');
 const router = express.Router();
 const writeHeader = require('../shared_functions/header');
 
+/* Start of Handlebars helpers */
+const formatPrice = (price) => {
+    return `\$${Number(price).toFixed(2)}`
+};
+
+const formatMultiplicationPrice = (product) => {
+    return `\$${(Number(product.price)*Number(product.quantity)).toFixed(2)}`;
+};
+
+const calculateTotal = (productList) => {
+    let total = 0;
+    for(product of productList){
+        if(product){
+            total += Number(product.price)*Number(product.quantity);
+        }
+    }
+    return `\$${Number(total).toFixed(2)}`;
+};
+
+const formatNewQty = (productId) => {
+    return `newqty${productId}`;
+};
+
+const formatKeydown = (productId) => {
+    return `handleKeyDown(event,${productId})`;
+};
+
+const formatRemovecart = (productId) => {
+    return `removecart?id=${productId}`;
+};
+
+const formatProductButtonId = (productId) => {
+    return `updateProductButton${productId}`;
+};
+
+const formatOnClick = (productId) => {
+    return `updateProduct(${productId},document.cart1.newqty${productId}.value)`;
+};
+/* End of Handlebars helpers */
+
 router.get('/', function(req, res, next) {
-    
-    writeHeader(res, `Your Shopping Cart`, `showcart`);
-    
-    res.write(
-        `
-        <script>
-        
-        function updateProduct(id, newqty) {
-            window.location="updatecart?".concat("id=", id, "&qty=", newqty);
-        }
-
-        function handleKeyDown(event, productId) {
-            let enterKeyCode = 13;
-            let keyCode = event.keyCode;
-            let buttonId = "updateProductButton".concat(productId);
-            if (keyCode === enterKeyCode) {
-                document.getElementById(buttonId).click();
-                event.preventDefault();
-            }
-        }
-
-        </script>
-        `
-    ); // JavaScript to retrieve input and button info
-
-    /* Start of utilities to display cart */
-    let createQuantityInput = (product) => {
-        return `
-            <input 
-                type="text" 
-                name="newqty${product.id}" 
-                size="3" value="${product.quantity}"
-                onkeydown = "handleKeyDown(event, ${product.id})" 
-            > 
-        `;
-    };
-
-    let createUpdateButton = (product) => {
-        return `
-            <button
-                id = "updateProductButton${product.id}"
-                type="button" 
-                onclick="updateProduct(${product.id}, document.cart1.newqty${product.id}.value)" value="Update Quantity"
-            >
-            Update Quantity
-            </button>
-        `;
-    };
-
-    let createSingleProductEntry = (product) => {
-        
-        if(!product){
-            return '';
-        }
-
-        return `
-            <tr>
-                <td>${product.id}</td>
-                <td>${product.name}</td>
-                <td align="center">${createQuantityInput(product)}</td>
-                <td align="right">\$${Number(product.price).toFixed(2)}</td>
-                <td align="right">\$${(Number(product.price)*Number(product.quantity)).toFixed(2)}</td>
-                <td align="right" style="padding-left: 5px;"> 
-                    <a href="removecart?id=${product.id}"> Remove Item From Cart </a> 
-                </td>
-                <td align="right" style="padding-left: 5px;">
-                    ${createUpdateButton(product)}
-                </td>
-            </tr>
-        `;
-    };
-
-    let createProductEntries = (productList) => {
-        return productList.map(createSingleProductEntry).join("\n");
-    };
-
-    let calculateTotal = (productList) => {
-        let total = 0;
-        for(product of productList){
-            if(product){
-                total += Number(product.price)*Number(product.quantity);
-            }
-        }
-        return total;
-    };
-
-    let writeShoppingCart = (res, productList) => {
-        if(productList){
-            res.write(
-                `<h1>Your Shopping Cart</h1>
-                
-                <form name="cart1">
-                    <table class="dragon-table">
-                        <thead>
-                            <tr>
-                                <th>Product Id</th> <th>Product Name</th> <th>Quantity</th>
-                                <th>Price</th> <th>Subtotal</th> <th> </th> <th> </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            ${createProductEntries(productList)}
-                            <tr> 
-                                <td colspan="6" align="right"> <b>Order Total</b> </td> 
-                                <td align="right">\$${calculateTotal(productList).toFixed(2)}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </form1>
-
-                <h2><a href="checkout">Check Out</a></h2>
-                <h2><a href="listprod">Continue Shopping</a></h2>
-                `
-            );
-        }
-        else {
-            res.write(
-                `
-                <h1> Your shopping cart is empty! </h1>
-                <h2><a href="listprod">Continue Shopping</a></h2>
-                `
-            );
-        }
-    };
-    /* End of utilities to display cart */
  
-    writeShoppingCart(res, req.session.productList);
-    res.end();
+    res.render('showcart', {
+        title: 'Your Shopping Cart',
+        pageActive: {'showcart': true},
+        productList: req.session.productList,
+        helpers: {
+            formatPrice,
+            formatMultiplicationPrice,
+            calculateTotal,
+            formatNewQty,
+            formatKeydown,
+            formatRemovecart,
+            formatProductButtonId,
+            formatOnClick
+        }
+    });
+
 });
 
 module.exports = router;
