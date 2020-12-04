b3bf9ceab30fc91138fc69ccc1d7ab9b3014d41f
Ivan Carvalho <ivan.carvalho@alumni.ubc.ca>
2020-12-03 20:19:15 -0800

    Initial check-in in back-end side validation

--- routes/createuser.js
+++ routes/createuser.js
@@ -2,6 +2,34 @@ const express = require('express');
 const router = express.Router();
 const sql = require('mssql');
 
+/* Start of back-end validator functions */
+const validatePasswordMatch = (password1, password2) => {
+    return password1 === password2;
+};
+
+const validateUserid = () => {
+    return true;
+};
+
+const validatePhoneNumber = (phoneNumber, optional=true) => {
+    if(phoneNumber === '' || phoneNumber === null || phoneNumber === undefined) {
+        return optional;
+    }
+    const matchedValue = phoneNumber.match(/^[+]?[\s./0-9]*[(]?[0-9]{1,4}[)]?[-\s./0-9]*$/g);
+    return !!phoneNumber;
+}
+
+const validatePostalCode = (postalCode) => {
+    const matchedValue = postalCode.match(/^[A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d$/);
+    return !!matchedValue;
+}
+
+const validateEmail = (email) => {
+    const matchedValue = email.match(/^\S+@\S+$/);
+    return !!matchedValue;
+};
+/* End of back-end validator functions */
+
 router.get('/', function(req, res, next) {
 
     let username = req.session.authenticatedUser;
@@ -15,14 +43,45 @@ router.get('/', function(req, res, next) {
     else {
 	    res.render('createuser/index', {
 	        title: 'DBs and Dragons Create User Account Page',
+            userWarning: req.session.createUserWarning,
 	        pageActive: {'createuser': true},
 	    });
+
+        if (req.session.createUserWarning) {
+            delete req.session.createUserWarning;
+        }
+
     }
 });
 
 router.post('/validate', function(req, res, next) {
 
+    let body = req.body;
 	console.log(req.body);
+
+    if(!validatePasswordMatch(body.password, req.body.confirmpassword)) {
+        req.session.createUserWarning = "Error: your password confirmation does not match";
+        res.redirect(`/createuser`);
+        return;
+    }
+
+    if(!validateEmail(body.email)){
+        req.session.createUserWarning = "Error: Invalid email address";
+        res.redirect(`/createuser`);
+        return;
+    }
+
+    if(!validatePhoneNumber(body.phonenumber)){
+        req.session.createUserWarning = "Error: Invalid Phone Number";
+        res.redirect(`/createuser`);
+        return;
+    }
+
+    if(!validatePostalCode(body.postalcode)){
+        req.session.createUserWarning = "Error: Invalid Postal Code";
+        res.redirect(`/createuser`);
+        return;
+    }
 
     let pool;
     (async function() {
