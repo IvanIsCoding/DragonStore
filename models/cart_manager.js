2d51826313083f1524308a7bda433c4c19b7d77f
Elias Pinno <pucas946@gmail.com>
2020-12-05 13:45:08 -0800

    Created cart manager with addItem
    Manages cart DDL and session with various functions

---
+++ models/cart_manager.js
@@ -0,0 +1,99 @@
+const sql = require('mssql');
+
+// Update sessional variable and db with new cart item
+const addItem = async (session, pool, id, name, price) => {
+    // session is req.session
+    // Update sessional variable
+    if (!session.productList) {
+        productList = [];
+    } else {
+        productList = session.productList;
+    }
+
+    if (productList[id]){
+        productList[id].quantity = productList[id].quantity + 1;
+    } else {
+        productList[id] = {
+            "id": id,
+            "name": name,
+            "price": price,
+            "quantity": 1
+        };
+    }
+    session.productList = productList;
+    // If the user is not logged in, we do not update the DB
+    if(!isLoggedIn(session)){
+        console.log("Not logged in");
+        return;
+    }
+    // Update db with our cart information
+    let sql = `
+    INSERT INTO incart(orderId,productId,quantity,price) 
+    VALUES(@oid,@pid,@qty,@price)
+    `
+    const psCart = new sql.PreparedStatement(pool);
+    psCart.input("cid",sql.Int);
+    psCart.input("pid",sql.Int);
+    psCart.input("qty",sql.Int);
+    psCart.input("price",sql.Decimal);
+
+};
+
+// Remove an item from our sessional and db cart
+const removeItem = async () => {
+
+};
+
+// adjust the quantity of our sessional and db cart
+const updateQty = async () => {
+
+};
+
+// On log in, load our stored cart from the db
+const loadCart = async (session) => {
+    if(!isLoggedIn(session)){ // This should never happen, but just in case
+        return;
+    }
+
+    if(!session.productList){ // There is no session cart yet
+        // Get cart from DB for this user, set the sessional cart to be it
+    }else{
+        mergeCart(session)
+    }
+
+// if cart in db is empty: pick session cart
+// if not empty: idk man, good luck
+};
+
+// On checkout, clear sessional cart and db cart for this customer
+const clearCart = async (session,pool) => {
+
+};
+
+const getSessionCart = (session) => {
+    return session.productList;
+};
+
+// Check if this user is logged in
+const isLoggedIn = (session) =>{
+    //req.session.authenticatedUser = authenticatedUser;
+    return session.authenticatedUser;
+};
+
+// Merge the cart as stored in the db with the sessional cart
+const mergeCart = (session) => {
+
+}
+
+// Get the cart as stored in the database
+const getDBCart = (session, pool) => {
+
+}
+
+module.exports = {
+    addItem,
+    removeItem,
+    updateQty,
+    loadCart,
+    clearCart
+};
