0d315da00b73177a037023a1ccbaec28ea3f728a
Elias Pinno <pucas946@gmail.com>
2020-12-06 07:32:48 -0800

    Added error handling to validPayment and validShipment

--- routes/validPayment.js
+++ routes/validPayment.js
@@ -2,15 +2,37 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../auth');
 const sql = require('mssql');
+const validate = require('../models/validation');
 
 router.post('/', function(req, res, next) {
     let body = req.body;
+
     valid = true
+    if(!validate.validatePaymentMethod(body.paymentMethod)){
+        console.log("bad method")
+        valid=false;
+    }
+    if(!validate.validateCardNumber(body.paymentNumber)){
+        console.log("bad number")
+        valid=false;
+    }
+    if(!validate.validatePaymentType(body.paymentType)){
+        console.log("bad type")
+        valid=false;
+    }
+    if(!validate.validateExpiryDate(body.expiryDate)){
+        console.log("bad date")
+        valid=false;
+    }
+    if(!req.session.shipmentinfo){ // got here without getting shipment info first?
+        valid=false;
+    }
     
     if(!valid){
         res.render('inputPayment',{
             invalidPayment: true
         });
+        return;
     }else{
         paymentInfo = {
             paymentMethod: body.paymentMethod,
