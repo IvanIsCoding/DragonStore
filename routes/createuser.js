22f314b8f713a04e40bb10e792988b79144abbcb
Ivan Carvalho <ivan.carvalho@alumni.ubc.ca>
2020-12-06 10:38:57 -0800

    Fix wrong import

--- routes/createuser.js
+++ routes/createuser.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const sql = require('mssql');
-const validation = require('mssql')
+const validation = require('../models/validation');
 
 
 router.get('/', function(req, res, next) {
